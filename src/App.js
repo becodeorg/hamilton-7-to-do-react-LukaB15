974512a6bd5b631920763fe62dc2dca78ab8b91b
LukaB15 <lukabrabant15@gmail.com>
2022-11-18 12:11:26 +0100

    step2

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import List from './List';
 
 function App() {
   return (
-    <div className="Todoapp">
+    <>
       <header className="Todoapp-header">
 
         <h1>My To do App</h1>
-
+    
       </header>
 
       <Add />
       <Submit />
       <List />
-    </div>
+    </>
      
   );
 }
+
 
 
 
--- src/List.js
+++ src/List.js
@@ -1,21 +1,20 @@
+import React, { useState } from "react";
 import './List.css';
-function List() {
-    return (
-        <div className='todoapp-list'>
-        <h3>Todos</h3>
-          <label>
-              <input type="checkbox"/>
-              Learn React
-          </label>
-          <br/>
-          <label>
-              <input type="checkbox"/>
-              Be awesome !
-          </label>
-      </div>
-    );
-  }
+
+export default function List() {
+    const initialTodos = [{id:1, name:"Todo1",complete:false}];
+    const [todos, setTodos] = useState(initialTodos);
+
+
+  return (
+    <ul>
+    {todos.map((todo) => (
+      <li key={todo.id}>
+        <input type="checkbox" defaultChecked={todo.complete}  /> {todo.name} 
+      </li>
+    ))}
+  </ul>
+  );
+}
   
-  
-  
-  export default List;
+ 
